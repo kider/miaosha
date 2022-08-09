cd1dff2f7ac880d5c047dafcd5c3efc6c6842de3
chenhao26 <chenhao26@gome.com.cn>
2022-08-09 15:19:17 +0800

    删除不用的文件

--- miaosha-rpc/dubbo-consumer/src/main/java/com/geekq/dubbo/springboot/consumer/config/TCCConfig.java
+++
@@ -1,7 +0,0 @@
-package com.geekq.dubbo.springboot.consumer.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class TCCConfig {
-}
--- miaosha-rpc/dubbo-provider/src/main/java/com/geekq/dubbo/springboot/privoder/config/TCCConfig.java
+++
@@ -1,7 +0,0 @@
-package com.geekq.dubbo.springboot.privoder.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class TCCConfig {
-}
